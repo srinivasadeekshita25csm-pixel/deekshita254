bee6b25d20fa1533709981e7e84fa90d5764891a
srinivasadeekshita25csm-pixel <srinivasadeekshita.25.csm@anits.edu.in>
2026-08-13 21:21:38 +0530

    Update multiple2.java

--- practice programs/inheritance/multiple2.java
+++ practice programs/inheritance/multiple2.java
@@ -1,1 +1,40 @@
+package inheritance;
 
+
+class First{
+	int x =10;
+	float cal_si(float p, int t,float roi) {
+		float si = (p*t*roi)/100;
+		return si;
+	}
+}
+class Second extends First {
+	@Override
+	float cal_si(float p, int t,float roi) {
+		float si = (p*t*roi)/10;
+		return si;
+		}
+	void access() {
+		float result = cal_si(10000,3,23.2f);
+		System.out.println("Si value is:" + result);
+	
+}}
+class Three extends Two {
+	@Override
+	float cal_si(float p, int t,float roi) {
+		float si = (p*t*roi)/1000;
+		return si;
+		}
+	void access() {
+		float result = cal_si(10000,3,23.2f);
+		System.out.println("Si value is:" + result);
+	
+}}
+
+
+public class Multiple2 {public static void main(String[] args) {
+	Three three = new Three();
+	three.access();
+}
+
+}
