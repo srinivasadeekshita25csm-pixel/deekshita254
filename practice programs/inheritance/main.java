bc91800f4016fa0c17761b7feffd74f08ee96751
srinivasadeekshita25csm-pixel <srinivasadeekshita.25.csm@anits.edu.in>
2026-08-13 21:21:14 +0530

    Update main.java

--- practice programs/inheritance/main.java
+++ practice programs/inheritance/main.java
@@ -1,1 +1,24 @@
+package inheritance;
 
+//Parent class
+class Employee {
+ int salary = 50000;
+}
+
+//Child class
+class Programmer extends Employee {
+ int bonus = 10000;
+
+ void displayTotal() {
+     // "super.salary" refers to the salary variable in Employee
+     int totalPay = super.salary + this.bonus;
+     System.out.println("Total Pay: $" + totalPay);
+ }
+}
+
+public class Main {
+ public static void main(String[] args) {
+     Programmer p = new Programmer();
+     p.displayTotal();
+ }
+}
