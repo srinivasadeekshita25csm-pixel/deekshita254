cf1a383c2b6a2687105d66e557c11e108586ee54
srinivasadeekshita25csm-pixel <srinivasadeekshita.25.csm@anits.edu.in>
2026-08-13 21:20:47 +0530

    Update heirachy.java

--- practice programs/inheritance/heirachy.java
+++ practice programs/inheritance/heirachy.java
@@ -1,1 +1,35 @@
+package inheritance;
 
+// Parent Class
+class Employe {
+ double salary = 50000;
+ 
+ public void showSalary() {
+    salary += 5000;
+     System.out.println("Salary: $" + salary);
+ }
+}
+
+//Subclass 1
+class Developer extends Employe {
+ double bonus = 10000;
+}
+
+//Subclass 2
+class Designer extends Employe {
+ double bonus = 5000;
+}
+
+public class Heirachy {
+ public static void main(String[] args) {
+     Developer dev = new Developer();
+     Designer des = new Designer();
+     
+ 
+     dev.showSalary(); 
+     des.showSalary(); 
+ }
+}
+// expected output:
+// Salary: $55000.0
+// Salary: $55000.0
