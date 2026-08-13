c0ec7ff2a9b9523d8a49e233503bc71955382976
srinivasadeekshita25csm-pixel <srinivasadeekshita.25.csm@anits.edu.in>
2026-08-13 21:23:34 +0530

    Update vehicle.java

--- practice programs/inheritance/vehicle.java
+++ practice programs/inheritance/vehicle.java
@@ -1,1 +1,26 @@
+package javacore;
 
+//Parent class
+class Vehicle1 {
+ void start() {
+     System.out.println("The vehicle starts moving.");
+ }
+}
+
+//Child class
+class Car extends Vehicle1 {
+ @Override
+ void start() {
+     System.out.println("The car engine roars to life.");
+ }
+}
+
+public class Vehicle{
+ public static void main(String[] args) {
+     Vehicle1 myVehicle = new Vehicle1();
+     Car myCar = new Car();
+
+     myVehicle.start(); // Runs vehicle1
+     myCar.start();     // Runs car
+ }
+}
