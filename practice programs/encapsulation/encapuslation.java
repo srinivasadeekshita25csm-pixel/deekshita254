d74a30578dbf60aa48e636972e414e289d76048c
srinivasadeekshita25csm-pixel <srinivasadeekshita.25.csm@anits.edu.in>
2026-08-13 21:11:01 +0530

    Update encapuslation.java

--- practice programs/encapsulation/encapuslation.java
+++ practice programs/encapsulation/encapuslation.java
@@ -1,1 +1,36 @@
+package javacore;
 
+class A{
+	private int items;
+	 private int count;
+	 void set(){
+		 items=2;
+		 count=2;
+	 }
+		 int get() {
+		 return items+count;
+	 }
+	 class B extends A {
+		 String name ="kavya";
+		 private void name() {
+			 System.out.println("My name is:");
+		 }
+		 void sets() {
+			 name="aarush";
+		 }
+		 String gets(){
+			 return name;
+		 }
+	 }
+}
+public class Encapsulation {
+	public static void main(String[] args) {
+		A a= new A();
+		a.set();
+		System.out.println(a.get());
+		A.B b= a.new B();// we write A.B beacuse b is declared as an inner class of A
+		b.sets();
+		System.out.println(b.gets());
+	}
+
+}
