674ae3aa9dadeaa43a2cf2914a8fc2ebf36255b3
srinivasadeekshita25csm-pixel <srinivasadeekshita.25.csm@anits.edu.in>
2026-08-13 21:07:30 +0530

    Update condemo.java

--- practice programs/constructor/condemo.java
+++ practice programs/constructor/condemo.java
@@ -1,1 +1,32 @@
+package javacore;
 
+public class ConDemo {
+// class or instance variables
+	int rollNo;
+    String name;
+    float fee;
+	
+	// constructor 
+	public ConDemo() { 
+	 this.rollNo = 254;
+	 this.name= "ammu";
+	 this.fee= 5000.98F;
+	}
+	void sample(){
+		System.out.println("ROLL NO is:" + this.rollNo);
+		System.out.println("NAME is:" + this.name);
+		System.out.println("FEE is:" + this.fee);
+	}
+	void example() {
+		System.out.println("some other method named example");
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("This is main method..!");
+		ConDemo cd = new ConDemo();
+		cd.sample();
+		cd.example();
+	}
+		
+	}
