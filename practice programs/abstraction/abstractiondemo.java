fe1e3825aaf785c848f24ecfff87a324079439a1
srinivasadeekshita25csm-pixel <srinivasadeekshita.25.csm@anits.edu.in>
2026-08-13 21:04:34 +0530

    Update abstractiondemo.java

--- practice programs/abstraction/abstractiondemo.java
+++ practice programs/abstraction/abstractiondemo.java
@@ -1,1 +1,22 @@
+package inheritance;
+abstract class Base {
+	int x=9;
+	
+	void SayName( ) {
+		System.out.println("My name is deekshita");
+	}
+	abstract void SayAge();
+}
+public class AbstractDemo extends Base {
 
+	@Override
+	void SayAge() {
+		System.out.println("age from AbstractDemo is 19");
+	}
+public static void main(String[] args) {
+	AbstractDemo ad = new AbstractDemo();
+	System.out.println("value of x from Base is:" + ad.x);
+	ad.SayName();
+	ad.SayAge();
+}
+}
