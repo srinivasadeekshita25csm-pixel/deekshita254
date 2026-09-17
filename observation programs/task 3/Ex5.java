e5abacd64245801c519578579642bf664c62de2b
srinivasadeekshita25csm-pixel <srinivasadeekshita.25.csm@anits.edu.in>
2026-09-17 21:42:37 +0530

    Update Ex5.java

--- observation programs/task 3/Ex5.java
+++ observation programs/task 3/Ex5.java
@@ -1,12 +1,15 @@
-ackage exceptionex;
+package csm254;
 
-public class Ex6 {
-    public static void main(String[] args) {
-        try {
-            String s = "Java";
-            System.out.println(s.charAt(10));
-        } catch (StringIndexOutOfBoundsException e) {
-            System.out.println("String index error!");
-        }
-    }
-}
+public class SIOOB { 
+	    public static void main(String[] args) {
+
+	        try {
+	            String str = "Hello";
+
+	            System.out.println(str.charAt(10));
+	        }
+	        catch (java.lang.StringIndexOutOfBoundsException e) {
+	            System.out.println("String index is out of bounds.");
+	        }
+	    }
+	}
