d177488f4b82785c08092a5ecf621c075e3352e3
srinivasadeekshita25csm-pixel <srinivasadeekshita.25.csm@anits.edu.in>
2026-09-25 01:27:34 +0530

    Create dynamic arrays.java

---
+++ practice programs/dynamic arrays.java
@@ -0,0 +1,38 @@
+package typesofarray;
+
+public class DynamicArray {
+	
+
+	
+		public static void main(String[] args) {
+			
+//			dynamic array
+	 int x[] = {10,20,30,40,50};
+		// for(int i=0; i<x.length ;i++) {
+			// System.out.println(x[i]);
+		//}
+		//	}
+			
+			//for each loop
+			for(int b:x) {
+				System.out.println(b*2);
+			}
+			
+//			static array
+			int y[] = new int[5];
+			y[0]=100;
+			System.out.println("\n First elements os static array y:" + y[0]);
+		}
+	}
+		
+		
+	
+
+
+
+		
+		
+	
+
+
+v
